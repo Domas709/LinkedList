c82b805d59c203d60b1b933e5a6c6257159c97f6
Domas Stočkus <Domas.Sto@gmail.com>
2017-11-16 17:58:05 +0200

    CodingAssignments

--- BrokenLinkAssignment/DataStructures/SpecialLinkedList.cs
+++ BrokenLinkAssignment/DataStructures/SpecialLinkedList.cs
@@ -17,12 +17,15 @@ namespace DataStructures
     public class SpecialLinkedList<T> : IEnumerable<T>
     {
         private Node<T> _head;
-
-        public T Head
+        private int count;
+        private Node<T> tempNode;
+        private Node<T> lastNode;
+        
+        public T Head    
         {
             get
             {
-                throw new NotImplementedException(); // returns first element of list
+                return _head.Value;              
             }
         }
 
@@ -30,55 +33,232 @@ namespace DataStructures
         {
             get
             {
-                throw new NotImplementedException();
+                return count;
             }
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            return new Enumerator<T>(_head, count);
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            throw new NotImplementedException();
+            return GetEnumerator();
         }
 
         public void Add(T item)
         {
-            throw new NotImplementedException();
+            count++;           
+            var node = new Node<T>() {Value = item};
+            if(_head == null)
+            {
+                _head = node;              
+            }
+            else
+            {
+                tempNode.Next = node;
+            }
+            tempNode = node;
+            
         }
 
         public void Remove(T item)
         {
-            throw new NotImplementedException();
+            tempNode = _head;
+            lastNode = null;
+            int size = 0;
+
+            while (tempNode != null)
+            {
+                if (Equals(item, tempNode.Value) && size == 0)
+                {
+                    _head = tempNode.Next;
+                    count--;
+                    return;
+                }
+
+                if (Equals(item, tempNode.Value) && size > 0)
+                {
+                    lastNode.Next = tempNode.Next;
+                    count--;
+                    return;                    
+                }
+
+                size++;
+                lastNode = tempNode;
+                tempNode = tempNode.Next;               
+            }
+            throw new ElementNotFoundException(string.Format("The following item was not found in the list: {0}", item));         
         }
 
         public void RemoveAt(int index)
         {
-            throw new NotImplementedException();
+            tempNode = _head;
+            lastNode = null;
+            int size = 1;
+
+            if (index == 0)
+            {
+                _head = tempNode.Next;
+                count--;
+                return;
+            }
+
+            if (index >= 1 && index <= count)
+            {
+                while (tempNode != null)
+                {
+                    
+                    if (index == size)
+                    {
+                        lastNode.Next = tempNode.Next;
+                        count--;
+                        return;
+                    }
+                    size++;
+                    lastNode = tempNode;
+                    tempNode = tempNode.Next;
+                }
+            }
+            throw new IndexOutOfRangeException();           
         }
 
         public override string ToString()
         {
-            throw new NotImplementedException(); // should print all members separated by comma (CSV format)
+            Node<T> tempNode = _head;
+            string temp = "";
+            char[] MyChar = { ',' };
+
+            while (tempNode != null)
+            {
+                temp = temp + tempNode.Value + ",";               
+                tempNode = tempNode.Next;
+            }                                                   // should print all members separated by comma (CSV format)          
+            temp = temp.TrimEnd(MyChar);
+            return temp;
         }
 
         public string ToStringReverse()
         {
-            throw new NotImplementedException();// should print all members separated by comma in reverse order (figure the best and most performant way)
+            Node<T> tempNode = _head;
+            string temp = "";
+            char[] MyChar = { ',' };
+
+            while (tempNode != null)
+            {
+                temp = tempNode.Value + "," + temp;   // should print all members separated by comma in reverse order (figure the best and most performant way)
+                tempNode = tempNode.Next;
+            }         
+            temp = temp.TrimEnd(MyChar);
+            return temp;
         }
 
         public T this[int index]
         {
             get
-            {
-                throw new NotImplementedException();
+            {                
+                tempNode = _head;
+                lastNode = null;
+                int size = 0;
+
+                if (index >= 0 && index <= count)
+                {
+                    while (tempNode != null)
+                    {
+                        if (index == size)
+                        {
+                            return tempNode.Value;
+                        }
+                        size++;
+                        lastNode = tempNode;
+                        tempNode = tempNode.Next;
+                    }
+                }     
+                throw new IndexOutOfRangeException();                                                                          
             }
             set
             {
-                throw new NotImplementedException();
+                tempNode = _head;
+                lastNode = null;
+                int size = 1;
+
+                if (index >= 0 && index <= count)
+                {
+                    while (tempNode != null)
+                    {
+
+                        if (index == size)
+                        {
+                            Console.WriteLine("Success");
+                            tempNode.Value = value;
+                        }
+                        size++;
+                        lastNode = tempNode;
+                        tempNode = tempNode.Next;
+                    }
+                }
+                throw new IndexOutOfRangeException();
             }
+        }
+    }
+    public class Enumerator<T> : IEnumerator<T>
+    {
+        private Node<T> tempNode;
+        private int x = 0;
+        private int count;
+
+        public Enumerator(Node<T> head, int count)
+        {
+            tempNode = head;
+            this.count = count;
+        }
+
+        public T Current
+        {
+            get
+            {
+                if (tempNode == null)
+                {
+                    return default(T);
+                }
+                return tempNode.Value;
+            }
+        }
+
+        object IEnumerator.Current
+        {
+            get { return Current; }
+        }
+
+        public void Dispose()
+        {
+            tempNode = null;
+        }
+
+        public bool MoveNext()
+        {
+            if (x != 0)
+            {
+                if (count == x)
+                    return false;
+                tempNode = tempNode.Next;                
+            }
+            x++;
+
+            return true;
+        }
+
+        public void Reset()
+        {
+            tempNode = null;
+        }
+    }
+    public class ElementNotFoundException : Exception
+    {
+        public ElementNotFoundException(string message)
+           : base(message)
+        {
         }
     }
 }
--- BrokenLinkAssignment/Program.cs
+++ BrokenLinkAssignment/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DataStructures;
+using DataStructures.Tests;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,8 +19,38 @@ namespace BrokenLinkAssignment
             // add 100 random members to it
             // print all 100 members to console
 
-            // var list = new SpecialLinkedList<int>();
-            // Console.WriteLine(list.ToString());
+            var list = new SpecialLinkedList<int>();
+            var test = new SpecialLinkedListTests();
+            try
+            {
+                test.CanAddMember();
+                test.CanSetHeadElement();
+                //test.ThrowsOutOfIndex();
+                test.RetrievesMembersByIndex();
+                test.CanRemoveMember();
+                test.CanRemoveMemberByIndex();
+                test.CanPrintEmptyList();
+                test.SupportsNullValues();
+                test.CanPrintMembers();
+                test.CanPrintMembersReverse();
+                test.CanLoopUsingForeach();              
+            }
+            catch(IndexOutOfRangeException)
+            {
+                Console.WriteLine("The provided index is out of range");
+            }
+            catch (ElementNotFoundException)
+            {
+                Console.WriteLine("The provided element does not exist in the list");
+            }
+
+            for (int i = 0; i < 100; i++)
+                list.Add(i);
+
+            Console.WriteLine(list.ToString());
+            Console.ReadKey();
+
+
         }
     }
 }
